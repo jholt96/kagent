c8d5bf205479d3070411c77cf6e5b22e3d638044
Collin Walker <lets-call-n-walk@users.noreply.github.com>
2026-03-02 16:29:55 -0500

    rename auth mode from "proxy" to "trusted-proxy"
    Clarifies that this mode explicitly trusts the upstream proxy's JWT
    validation and does not re-validate tokens.
    
    Signed-off-by: Collin Walker <lets-call-n-walk@users.noreply.github.com>

--- go/core/cmd/controller/main.go
+++ go/core/cmd/controller/main.go
@@ -41,7 +41,7 @@ func main() {
 
 func getAuthenticator(authCfg struct{ Mode, UserIDClaim string }) pkgauth.AuthProvider {
 	switch authCfg.Mode {
-	case "proxy":
+	case "trusted-proxy":
 		return auth.NewProxyAuthenticator(authCfg.UserIDClaim)
 	default:
 		return &auth.UnsecureAuthenticator{}
